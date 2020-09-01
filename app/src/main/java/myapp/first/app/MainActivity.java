b8b3d3c76e8b554ba5876a0a71a65357bce23026
DESKTOP-2GUSI25\Wolf <pkuongnguyen2k@gmail.com>
2020-09-01 08:58:31 +0700

    + Init Project android

---
+++ app/src/main/java/myapp/first/app/MainActivity.java
@@ -0,0 +1,16 @@
+package myapp.first.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+        val list = "dasd";
+    }
+}
